394b3704bd5f001641e51d15bfd28cb40bc0d985
Admin <admin@brooksyp.local>
2017-10-20 12:36:03 -0400

    adding prj

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController} from 'ionic-angular'; //added Loader
+import {HttpProvider} from '../../providers/http/http'; //importing provider 
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[HttpProvider]
 })
+
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  eventData: any;
+  loading: any;
 
+  //injecting HttpProvider with angular 2 dependancy in ctor + Loader
+  constructor(public navCtrl: NavController, private httpProvider:HttpProvider, public loadingCtrl: LoadingController) {
+
+    this.loading = this.loadingCtrl.create({
+      content: `
+      <ion-spinner ></ion-spinner>`
+    });
+
+    this.getdata();
   }
+
+getdata(){
+
+
+this.httpProvider.getJsonData().subscribe(
+  data => {
+console.log(data);
+this.eventData=JSON.parse(JSON.stringify(data));
+console.log(this.eventData);
+
+},
+err =>{
+console.error("Error : " +err);
+},
+() => {
+console.log('getData completed');
+}
+);
+}
+
 
 }
